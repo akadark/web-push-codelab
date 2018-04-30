9ed5aeeff85975f4716178c0ea9cf916218b3a47
Chungmin Park <chuck.m.park@gmail.com>
2018-04-30 22:07:11 +0900

    update first step

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,7 +21,7 @@
 
 'use strict';
 
-const applicationServerPublicKey = '<Your Public Key>';
+const applicationServerPublicKey = 'BCIJlJDMO8u8zZ-E8iBb_oK1N3rgyyEOm-kZow7QxZE9-CKU1cVju6iYVCPQRAvVEUczpwUrfCibOuaAnlC4Y7o';
 
 const pushButton = document.querySelector('.js-push-btn');
 
@@ -41,4 +41,131 @@ function urlB64ToUint8Array(base64String) {
     outputArray[i] = rawData.charCodeAt(i);
   }
   return outputArray;
+}
+
+// 현재 브라우저에서 서비스 워커와 푸시 메시지를 지원하는지 확인하고
+// 지원한다면 sw.js 파일을 등록
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  navigator.serviceWorker.register('sw.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+
+    swRegistration = swReg;
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
+}
+
+// 사용자가 현재 구독한 상태인지 확인 
+function initialiseUI() {
+  pushButton.addEventListener('click', function() {
+    // 사용자가 푸시 버튼을 클릭할 때 푸시를 구독하는 데 시간이 좀 걸릴 수 있으므로 
+    // 먼저 버튼을 비활성화하여 구독하는 동안 사용자가 버튼을 다시 클릭할 수 없도록 함
+    pushButton.disabled = true;
+    if (isSubscribed) {
+      // TODO: Unsubscribe user
+    } else {
+      subscribeUser(); // 사용자가 현재 구독하지 않은 상태임을 알 때 subscribeUser()를 호출
+    }
+  });
+
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      console.log('User is NOT subscribed.');
+    }
+
+    updateBtn();
+  });
+}
+
+// 사용자가 구독한 상태인지 여부에 따라 버튼을 활성화하고 텍스트를 변경
+function updateBtn() {
+  if (isSubscribed) {
+    pushButton.textContent = 'Disable Push Messaging';
+  } else {
+    pushButton.textContent = 'Enable Push Messaging';
+  }
+
+  pushButton.disabled = false;
+}
+
+// 서비스 워커가 등록될 때 initialiseUI()를 호출
+navigator.serviceWorker.register('sw.js')
+.then(function(swReg) {
+  console.log('Service Worker is registered', swReg);
+
+  swRegistration = swReg;
+  initialiseUI();
+})
+
+// 구독하기
+function subscribeUser() {
+  // 구독 호출의 예상 입력을 알아내기 위해 
+  // 기본 64 URL 안전 인코딩된 애플리케이션 서버의 공개 키를 취하여 UInt8Array로 변환
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true, // userVisibleOnly 매개변수는 기본적으로 푸시가 전송될 때마다 알림을 표시하도록 허용
+    applicationServerKey: applicationServerKey
+  })
+  .then(function(subscription) {
+    console.log('User is subscribed:', subscription);
+
+    updateSubscriptionOnServer(subscription);
+
+    isSubscribed = true;
+
+    updateBtn();
+  })
+  .catch(function(err) {
+    console.log('Failed to subscribe the user: ', err);
+    updateBtn(); // 버튼이 다시 활성화되고 알맞은 텍스트가 있는지 확인
+  });
+}
+
+// 실제 애플리케이션에서 백엔드로 구독을 보내는 메서드
+// 여기서는 나중에 도움이 되도록 UI에 구독을 출력
+function updateSubscriptionOnServer(subscription) {
+  // TODO: Send subscription to application server
+
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails =
+    document.querySelector('.js-subscription-details');
+
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    subscriptionDetails.classList.remove('is-invisible');
+  } else {
+    subscriptionDetails.classList.add('is-invisible');
+  }
+}
+
+/* 거부된 권한 처리 */
+function updateBtn() {
+  if (Notification.permission === 'denied') { // 사용자가 알림 권한을 차단한 경우
+    pushButton.textContent = 'Push Messaging Blocked.';
+    pushButton.disabled = true;
+    updateSubscriptionOnServer(null);
+    return;
+  }
+
+  if (isSubscribed) {
+    pushButton.textContent = 'Disable Push Messaging';
+  } else {
+    pushButton.textContent = 'Enable Push Messaging';
+  }
+
+  pushButton.disabled = false;
 }
--- app/sw.js
+++ app/sw.js
@@ -20,3 +20,22 @@
 /* eslint-env browser, serviceworker, es6 */
 
 'use strict';
+
+/*
+*  푸시 메시지를 트리거할 때 브라우저는 푸시 메시지를 수신하고 푸
+*  시의 대상이 되는 서비스 워커를 발견한 후 그 서비스 워커를 깨워 푸시 이벤트를 발송합니다. 
+*  이 이벤트를 수신 대기하고 이벤트를 수신했을 때 그 결과로서 알림을 표시해야 합니다.
+*/
+self.addEventListener('push', function(event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+  
+    const title = 'Push Codelab';
+    const options = {
+      body: 'Yay it works.',
+      icon: 'images/icon.png',
+      badge: 'images/badge.png'
+    };
+  
+    event.waitUntil(self.registration.showNotification(title, options));
+  });
